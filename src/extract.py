00664385d3c1837d6e08a5ba0db286d734acb0dd
gholamimaryam@gmail.com <gholamimaryam@gmail.com>
2026-04-15 13:42:59 +0000

    Add try youtube APIs notebook

--- src/extract.py
+++ src/extract.py
@@ -67,16 +67,15 @@ def fetch_playlist_videos(youtube, playlist_id: str):
 def fetch_video_statistics(youtube, video_ids: list):
     """
     Fetch video statistics + content details (including duration)
-    in batches of 50 (API limit)
     """
     stats = []
 
     for i in range(0, len(video_ids), 50):
         response = youtube.videos().list(
-            part="statistics,contentDetails",  # ✅ ADD THIS
+            part="statistics,contentDetails",
             id=",".join(video_ids[i:i+50])
         ).execute()
 
-        stats.extend(response["items"])
+        stats.extend(response.get("items", []))
 
     return stats
